686434069b2a4a719e51dda8d71c8003fc4eea7d
Kishimura <tiagowinchesterx@hotmail.com>
2026-05-29 17:44:41 -0300

    Inicia interface gráfica com Flet

--- flet_app.py
+++ flet_app.py
@@ -1,17 +1,40 @@
 import flet as ft
+from horimetro_app import(
+    validar_horimetro,
+    converter_para_minutos,
+    calcular_diferenca,
+    formatar_tempo,
+)
 
 def main (page: ft.Page):
 
-   campo = ft.TextField(
-       label="Horímetro Inicial"
-   )
-   campo_2 = ft.TextField(
-       label="Horímetro Final"
-   )
+   page.title = "Calculadora de Horímetro"
+
+
+   horas_inicial = ft.TextField(label="Horímetro onicial")
+   minutos_inicial = ft.TextField(label="Minutos inicial", max_lenght=2)
+
+   horas_final = ft.TextField(label="Horímetro final")
+   minutos_final = ft.TextField(label="Minutos final", max_lenght=2)
+
+   resultado = ft.Text("")
+
    botao = ft.ElevatedButton("Calcular")
 
-   page.add(campo,
-            campo_2,
-            botao)
+
+   def calcular(e):
+       inicial = f"{horas_inicial.value}:{minutos_inicial.value}"
+       final = f"{horas_final.value}:{minutos_final.value}"
+
+       resultado_inicial, erro_inicial = validar_horimetro(inicial)
+       resultado_final, erro_final = validar_horimetro(final)
+
+
+
+
+
+   page.add()
 
 ft.app(target=main)
+
+
--- horimetro_app.py
+++ horimetro_app.py
@@ -158,4 +158,5 @@ def main():
                 else:
                     print("Digite apenas S ou N.")
 
-main()
+if __name__ == "__main__":
+    main()
